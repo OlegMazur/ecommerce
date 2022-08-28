ca54b23e10a8432cfb76c161357c21e8d32d6bb6
OlegMazur <olegykphotographer@gmail.com>
2022-08-28 03:30:38 +0300

    2

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const {Sequelize}=require('sequelize')
 module.exports=new Sequelize(
-    "dbf7djvih2tl9t",
-    "kfyggdrtvkltmc",
-    "c9c4b76179eb97d500c10d05dafab2ac909d559ea316296af8d88e90c1a42ec5",
+    dbf7djvih2tl9t,
+    kfyggdrtvkltmc,
+    c9c4b76179eb97d500c10d05dafab2ac909d559ea316296af8d88e90c1a42ec5,
     {
-        dialect:'postgres',
-        host:"ec2-176-34-215-248.eu-west-1.compute.amazonaws.com",
+        dialect:"postgres",
+        host: "ec2-176-34-215-248.eu-west-1.compute.amazonaws.com",
         port:5432
     }
     // process.env.DB_NAME,
