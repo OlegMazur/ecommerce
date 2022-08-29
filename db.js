1b4620ba5fff420c3fb8fa2593090075ac1fbeec
OlegMazur <olegykphotographer@gmail.com>
2022-08-29 04:22:29 +0300

    change dbconfig

--- db.js
+++ db.js
@@ -2,16 +2,27 @@
 const {Sequelize}=require('sequelize')
 require('dotenv').config()
 module.exports=new Sequelize({
-    database:process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    database:"m111087_db",
+    username: "m111087_dbuser",
+    password: "g=4K0Q(uJj!0",
      
       
-        dialect:'postgres',
+        dialect:"mysql",
      host:process.env.DB_HOST,
       port:process.env.DB_PORT
       
 })
+// module.exports=new Sequelize({
+//     database:process.env.DB_NAME,
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+     
+      
+//         dialect:'postgres',
+//      host:process.env.DB_HOST,
+//       port:process.env.DB_PORT
+      
+// })
 
 // module.exports=new Sequelize({
     
